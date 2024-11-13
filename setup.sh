1017f992570a0ebebbe12191aa66b151988fcc52
Player1444 <94734830+Player124413@users.noreply.github.com>
2024-11-13 15:44:33 +0300

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-stage=2
-stop_stage=2
+stage=${1:-0}
+stop_stage=${2:-999}
 
 PYTHON_ENVIRONMENT=contentvec
-CONDA_ROOT=/mnt/anaconda3
+CONDA_ROOT=/home/$(whoami)/miniconda3
 
 source ${CONDA_ROOT}/etc/profile.d/conda.sh
 
@@ -14,7 +14,7 @@ CODE=${cwd}
 
 if [ $stage -le 0 ] && [ $stop_stage -ge 0 ]; then
     echo "Stage 0: Install conda environment..."
-    
+
     conda create --name ${PYTHON_ENVIRONMENT} python=3.7 -y
 fi
 
@@ -22,7 +22,7 @@ conda activate ${PYTHON_ENVIRONMENT}
 
 if [ $stage -le 1 ] && [ $stop_stage -ge 1 ]; then
     echo "Stage 1: Install fairseq and other dependencies..."
-    
+
     if [ ! -d fairseq ]; then
         git clone https://github.com/pytorch/fairseq.git --branch main --single-branch
     fi
@@ -38,17 +38,17 @@ if [ $stage -le 1 ] && [ $stop_stage -ge 1 ]; then
     fi
     # optionally do this if related error occurs
     python setup.py build_ext --inplace
-    
+
     python -m pip install scipy
     python -m pip install soundfile
     python -m pip install praat-parselmouth
     python -m pip install tensorboardX
-    
+
     cd ${cwd}
 fi
 
 if [ $stage -le 2 ] && [ $stop_stage -ge 2 ]; then
     echo "Stage 2: Copy model files to fairseq..."
     rsync -a contentvec/ fairseq/fairseq/
-    
+
 fi
